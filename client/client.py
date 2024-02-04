a66c94f4a6d635a4a382591fbd1c867b666b3e6c
ibsiany <ibsianyd@gmail.com>
2024-02-04 16:12:12 -0300

    feat: add send files and validate password

--- client/client.py
+++ client/client.py
@@ -10,93 +10,28 @@ udp.bind(('', 6000))
 
 print('Socket UDP na porta 6000')
 
-
 password = input('Digite a senha: ')
-udp.sendto(bytes(password, 'ascii'), dest)
+udp.sendto(bytes('password|' + password, 'ascii'), dest)
 
 msg_confirmation, cliente = udp.recvfrom(1024)
 print(cliente, msg_confirmation.decode('ascii'))
-if(msg_confirmation.decode('ascii') == 'PASSWORD INCORRECT'):
+
+if(msg_confirmation.decode('ascii') == 'SENHA INCORRETA'):
     print('Conexao finalizada, senha incorreta!')
     udp.close()
 else:
-    msg = input('Digite a mensagem: ')
-    udp.sendto(bytes(msg, 'ascii'), dest)
+    try:
+        msg_list, cliente = udp.recvfrom(1024)
+        print(cliente, msg_list.decode('ascii'))
 
-    msg_confirmation, cliente = udp.recvfrom(1024)
-    print(cliente, msg_confirmation.decode('ascii'))
+        arquivo = input('Digite o arquivo desejado: ')
+        udp.sendto(bytes('select_file|'+arquivo, 'ascii'), dest)
 
-    partition_length = input('Digite o tamanho das particoes: ')
-    udp.sendto(bytes(partition_length, 'ascii'), dest)
+        msg_arquivo, cliente = udp.recvfrom(1024)
+        print(cliente, msg_arquivo.decode('ascii'))
 
-    received_size, cliente = udp.recvfrom(1024)
+        
 
-    array_size = int(received_size.decode('ascii'))
+    except Exception as e:
+        print('erro errado: ' + str(e))
 
-    print(cliente, array_size)
-
-    array = []
-
-    for i in range(0, array_size):
-        array.append('')
-
-    print(array)
-
-    values = ['']
-    while values[0] != 'end':
-        payload, cliente = udp.recvfrom(1024)
-        values = payload.decode('ascii').split('|', 1)
-        if values[0] != 'end':
-            if (int(values[0]) >= 0) & (int(values[0]) < array_size):
-                array[int(values[0])] = values[1]
-
-    print(array)
-
-    fault_indexes = []
-
-    for i in range(len(array)):
-        if array[i] == '':
-            fault_indexes.append(i)
-
-    print(fault_indexes)
-
-    while len(fault_indexes) > 0:
-        fault_msg = ''
-        for i in range(len(fault_indexes)):
-            fault_msg = fault_msg + str(fault_indexes[i])
-            if i != len(fault_indexes) - 1:
-                fault_msg = fault_msg + '|'
-
-        udp.sendto(bytes(fault_msg, 'ascii'), dest)
-
-        values = ['']
-
-        while values[0] != 'fin?':
-            payload, cliente = udp.recvfrom(1024)
-            values = payload.decode('ascii').split('|')
-            print(values)
-            if values[0] != 'fin?':
-                if (int(values[0]) >= 0) & (int(values[0]) < array_size):
-                    array[int(values[0])] = values[1]
-
-        print(array)
-
-        fault_indexes = []
-        for i in range(len(array)):
-            if array[i] == '':
-                fault_indexes.append(i)
-
-
-
-    udp.sendto(bytes('fin', 'ascii'), dest)
-
-    udp.close()
-    print('Conexao finalizada')
-
-    mensagem = ''
-
-    for fragment in array:
-        mensagem = mensagem + fragment
-
-    print("Mensagem recebida: ")
-    print(mensagem)
--- server/server.py
+++ server/server.py
@@ -1,65 +1,64 @@
 import math
 import socket
+import os
 
+PASSWORD = '1234' # Senha de acesso ao servidor
 HOST = ''  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 udp.bind(orig)
+diretorio = './files'
 
 dest = ('localhost', 6000)
 
 print('Socket UDP na porta 5000')
 
-password, cliente = udp.recvfrom(1024)
+def listFiles(): # Função para listar arquivos disponíveis
+    arquivos = os.listdir(diretorio)
+    array = [];
+    for arquivo in arquivos:
+        if os.path.isfile(os.path.join(diretorio, arquivo)):
+            array.append(arquivo)
+    
+    return array;
 
-sentence = password.decode('ascii')
+files = listFiles();
 
-print(cliente, sentence)
 
-if(sentence == '1234'):
-    udp.sendto(bytes('PASSWORD OK', 'ascii'), dest)
+def check_password(sentence): # Função de verificação de senha
+    if(sentence == PASSWORD):
+        udp.sendto(bytes('SENHA OK', 'ascii'), dest)
+        list = ', '.join(listFiles())
+        udp.sendto(bytes(list, 'ascii'), dest)
+    else:
+        udp.sendto(bytes('SENHA INCORRETA', 'ascii'), dest)
 
-    msg, cliente = udp.recvfrom(1024)
+def select_file(filename):  # Função para selectionar arquivo
+    if filename not in files:
+        udp.sendto(bytes('Arquivo nao encontrado.', 'ascii'), dest)
+        return False
 
-    sentence = msg.decode('ascii')
+    udp.sendto(bytes('Arquivo sera enviado.', 'ascii'), dest)
+    send_file(filename);
 
-    print(cliente, sentence)
+def send_file(filename): # Função para enviar o arquivo selecionado
+    print(filename)    
+    return True;
 
-    udp.sendto(bytes('MSG RECEIVED', 'ascii'), dest)
+while True:
+    message, cliente = udp.recvfrom(1024)
+    sentence = message.decode('ascii')
+    print(cliente, sentence)    
 
-    partition_length, cliente = udp.recvfrom(1024)
+    idetify = sentence.split("|")[0]
+    message = sentence.split('|')[1]
 
-    partition_number = int(partition_length.decode('ascii'))
+    match idetify:
+        case "password":
+            check_password(message)
+        case 'select_file':
+            select_file(message)
 
-    array_size = math.ceil(len(sentence) / partition_number)
+    
 
-    udp.sendto(bytes(str(array_size), 'ascii'), dest)
-
-    array = []
-
-    for i in range(0, array_size):
-        array.append(sentence[(i * partition_number):((i * partition_number) + partition_number)])
-
-    print(array)
-
-    for i in range(array_size):
-        udp.sendto(bytes(str(i) + '|' + array[i], 'ascii'), dest)
-
-    udp.sendto(bytes('end', 'ascii'), dest)
-
-    indexes, cliente = udp.recvfrom(1024)
-
-    print(indexes.decode('ascii'))
-
-    while indexes.decode('ascii') != 'fin':
-        indexes_array = indexes.decode('ascii').split('|')
-        for i in indexes_array:
-            print(i + '|' + array[int(i)])
-            udp.sendto(bytes(i + '|' + array[int(i)], 'ascii'), dest)
-        udp.sendto(bytes('fin?', 'ascii'), dest)
-        indexes, cliente = udp.recvfrom(1024)
-
-    udp.close()
-else:
-    udp.sendto(bytes('PASSWORD INCORRECT', 'ascii'), dest)
